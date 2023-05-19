0b70deb2caec722d1bf0565c975cdc34d8ca0db1
RyanGoosling <goosev0801@gmail.com>
2023-05-19 15:31:48 +0300

    lab4 fix checker

--- lab4/app/app.py
+++ lab4/app/app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, session, request, redirect, url_for, f
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
 from mysql_db import MySQL
 import mysql.connector
+import re
 PERMITED_PARAMS = ['login', 'password', 'last_name', 'first_name', 'middle_name', 'role_id']
 EDIT_PARAMS = ['last_name', 'first_name', 'middle_name', 'role_id']
 
@@ -84,45 +85,69 @@ def extract_params(params_list):
     return params_dict
 
 def check_password(password):
+    wrong_point = ''
     has_uppercase = False
     has_lowercase = False
     has_digit = False
-    if len(password) > 7 and len(password) < 129:
-        for char in password:
-            if char.isalpha() or char.isdigit() or char in '~!@#$%^&*_-+()[]{}><\/|"\',.:;':
-                if char.isupper():
-                    has_uppercase = True
-                elif char.islower():
-                    has_lowercase = True
-                elif char.isdigit():
-                    has_digit = True
-            else:
-                return False
+    if len(password) > 7: 
+        if len(password) > 128:
+            wrong_point += 'Длина пароля не может быть больше 128 символов. '
     else: 
-        return False
-    return all([has_uppercase, has_lowercase, has_digit])
+        wrong_point += 'Длина пароля не может быть меньше 8 символов. '
+    for char in password:
+        if char.isalnum() or char in '~!@#$%^&*_-+()[]{}><\/|"\',.:;':
+            if char.isupper():
+                has_uppercase = True
+            elif char.islower():
+                has_lowercase = True
+            elif char.isdigit():
+                has_digit = True
+        else:
+            wrong_point += 'Пароль должен содержать только латинские или кириллические буквы, цифры или ~!@#$%^&*_-+()[]{}><\/|"\',.:; '
+    if not has_uppercase:
+        wrong_point += 'Пароль должен содержать минимум одну заглавную букву. '
+    if not has_lowercase:
+        wrong_point += 'Пароль должен содержать минимум одну строчную букву. '
+    if not has_digit:
+        wrong_point += 'Пароль должен содержать минимум одну цифру. '
+    return wrong_point
+
+def check_login(login):
+    wrong_point = ''
+    if len(login) < 5:
+        wrong_point += 'Длина логина не может быть меньше 5 символов. '
+    if not bool(re.match(r'^[a-zA-Z0-9]+$', login)):
+        wrong_point += 'Логин должен состоять только из латинских букв и цифр. '
+    return wrong_point
 
 @app.route('/users/create', methods=['POST'])
 @login_required
 def create_user():
     params = extract_params(PERMITED_PARAMS)
     wrong_params = []
+    wrong_login = ''
+    wrong_password = ''
     # print(params)
     for key, value in params.items():
         if not value and key != 'middle_name':
             wrong_params.append(key)
 
-    if params['login'] and (len(params['login']) < 5 or not params['login'].isalnum()):
-        wrong_params.append('incorrect_login')
+    
+    if params['login']:
+        wrong_login = check_login(params['login'])
+        if wrong_login:
+            wrong_params.append('incorrect_login')
 
-    if params['password'] and not check_password(params['password']):
-        wrong_params.append('incorrect_password')
+    if params['password']:
+        wrong_password = check_password(params['password'])
+        if wrong_password:
+            wrong_params.append('incorrect_password')
 
         
     # print(wrong_params)
     if wrong_params:
         flash('Заполните корректно форму', 'danger')
-        return render_template('users_new.html', user = params, roles_list = load_roles(), wrong_params = wrong_params)
+        return render_template('users_new.html', user = params, roles_list = load_roles(), wrong_params = wrong_params, wrong_login = wrong_login, wrong_password = wrong_password)
     
 
     query = 'INSERT INTO users(login, password_hash, last_name, first_name, middle_name, role_id) VALUES (%(login)s, SHA2(%(password)s, 256), %(last_name)s, %(first_name)s, %(middle_name)s, %(role_id)s);'
@@ -222,6 +247,7 @@ def load_user(user_id):
 @login_required
 def change_password(user_id):
     wrong_params = []
+    wrong_password = ''
     if request.method == 'POST':
         old_password = request.form['oldpassword'] or wrong_params.append('none_old_password')
         new_password = request.form['newpassword'] or wrong_params.append('none_new_password')
@@ -237,8 +263,11 @@ def change_password(user_id):
             # print(cursor.statement)
             user = cursor.fetchone()
 
-        if new_password and not check_password(new_password):
-            wrong_params.append('incorrect_new_password')
+        if new_password:
+            wrong_password = check_password(new_password)
+            if wrong_password:
+                wrong_params.append('incorrect_new_password')
+
         if new_password != repeat_password:
             wrong_params.append('repeat_password')
 
@@ -256,4 +285,4 @@ def change_password(user_id):
         elif not user:
             wrong_params.append('old_password')
 
-    return render_template('change_password.html', wrong_params = wrong_params)
+    return render_template('change_password.html', wrong_params = wrong_params, wrong_password = wrong_password)
