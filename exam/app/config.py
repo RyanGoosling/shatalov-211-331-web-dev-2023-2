7de332c7da5f04818f06ac5bd01efb9af95866e1
RyanGoosling <91082227+RyanGoosling@users.noreply.github.com>
2023-06-16 16:28:59 +0300

    Delete config.py

--- exam/app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-SECRET_KEY = 'secret-key'
-
-SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://std_1846_exam:pa$$w0rd@std-mysql.ist.mospolytech.ru/std_1846_exam'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_ECHO = True
-
-UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'media', 'images')
